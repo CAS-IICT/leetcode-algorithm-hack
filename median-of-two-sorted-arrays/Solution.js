129281fe872788878bf525d24f13bfc6965961ac
devilyouwei <devilyouwei@gmail.com>
2020-04-02 21:52:36 +0800

    add binary search to median

--- median-of-two-sorted-arrays/Solution.js
+++ median-of-two-sorted-arrays/Solution.js
@@ -3,7 +3,7 @@
  * @param {number[]} nums2
  * @return {number}
  */
-var findMedianSortedArrays = function(nums1, nums2) {
+var findMedianSortedArrays = function (nums1, nums2) {
     let arr = []
     let i = nums1.length == 0 ? Infinity : 0
     let j = nums2.length == 0 ? Infinity : 0
@@ -24,14 +24,61 @@ var findMedianSortedArrays = function(nums1, nums2) {
         : arr[parseInt(arr.length / 2)]
 }
 
-const test=[
-    [[1,2,3],[4,5,6]],
-    [[],[4]],
-    [[1],[]],
-    [[2,5,8],[1,1,1]],
-    [[3,5,11],[2,20,100]],
-    [[1,2,8],[1,11,7,8,9,12]]
+findMedianSortedArrays = function(nums1, nums2) {
+    if(nums1.length>nums2.length){
+        let tmp=nums1
+        nums1=nums2
+        nums2=tmp
+    }
+    let start = 0
+    let end = nums1.length
+    let i = 0
+    let j = 0
+    let lefti = 0
+    let leftj = 0
+    let righti = 0
+    let rightj = 0
+    while(i<=nums1.length && i>=0){
+        i = start>end?start:parseInt((start+end)/2)
+        j = parseInt((nums1.length+nums2.length+1)/2 - i)
+        
+        lefti = (i==0?-Infinity:nums1[i-1])
+        leftj = (j==0?-Infinity:nums2[j-1])
+        righti = (i==nums1.length?Infinity:nums1[i])
+        rightj = (j==nums2.length?Infinity:nums2[j])
+        
+        if(lefti<=rightj && leftj<=righti) break
+        else{
+            if(lefti>rightj) end=i-1
+            if(righti<leftj) start=i+1
+        }
+    }
+    maxLeft = Math.max(lefti,leftj)
+    minRight = Math.min(righti,rightj)
+    let result = (maxLeft+minRight)/2
+    return (nums1.length+nums2.length)%2==0?result:maxLeft
+}
+
+const test = [
+    [
+        [1, 2, 3],
+        [4, 5, 6]
+    ],
+    [[], [4]],
+    [[1], []],
+    [
+        [2, 5, 8],
+        [1, 1, 1]
+    ],
+    [
+        [3, 5, 11],
+        [2, 20, 100]
+    ],
+    [
+        [1, 2, 8],
+        [1, 11, 7, 8, 9, 12]
+    ]
 ]
 for (let i in test) {
-    console.log(findMedianSortedArrays(test[i][0],test[i][1]))
+    console.log(findMedianSortedArrays(test[i][0], test[i][1]))
 }
